57e5d4a88fc15fd6d433d9cb945b8f58410b9c36
kanade <melty0404@gmail.com>
2018-07-24 23:18:03 +0900

    add templates modify models

--- sns/models.py
+++ sns/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator, MinLeng
 
 class Message(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_owner')
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    group = models.ForeignKey("Group", on_delete=models.CASCADE, db_column='group_id', default=-1)
     content = models.TextField(max_length=200)
     share_id = models.IntegerField(default=-1)
     good_count = models.IntegerField(default=0)
@@ -47,10 +47,3 @@ class Good(models.Model):
     def __str__(self):
         return 'good for {0}(by{1})'.format(self.message, self.owner)
 
-
-def number_only(value):
-    if re.match(r'^[0-9]*$', value) == None:
-        raise ValidationError(
-            '%(value)s is not Number',
-            params={'value': value},
-        )
--- sns/urls.py
+++ sns/urls.py
@@ -3,12 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('<int:num>', views.index, name='index'),
-    # path('create', views.create, name='create'),
-    # path('edit/<int:num>', views.edit, name='edit'),
-    # path('delete/<int:num>', views.delete, name='delete'),
-    # path('find', views.find, name='find'),
-    # path('message/', views.message, name='message'),
-    # path('message/<int:page>', views.message, name='message'),
+    path('', views.index, name='index'),
+    path('groups', views.groups, name='groups'),
+    path('add', views.add, name='add'),
+    path('creategroup', views.create_group, name='creategroup'),
+    path('post', views.post, name='post'),
+    path('share/<int:share_id>', views.share, name='share'),
+    path('good/<int:good_id>', views.good, name='good'),
 ]

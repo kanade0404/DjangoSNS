a3383d0c811f822dbac6b7afd24746862761fb12
kanade <melty0404@gmail.com>
2018-09-23 16:38:41 +0900

    pass

--- KanadeSns/settings.py
+++ KanadeSns/settings.py
@@ -150,4 +150,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'melty0404@gmail.com'
-EMAIL_HOST_PASSWORD = 'buevdefeqhehqxwk'
+EMAIL_HOST_PASSWORD = 'password'

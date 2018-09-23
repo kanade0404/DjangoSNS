eb1526b47e530489605df7a8b6b88a65f76a01f4
kanade <melty0404@gmail.com>
2018-09-23 18:57:24 +0900

    パスワード再発行

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('signup/', views.UserCreate.as_view(), name='signup'),
     path('signup/done/', views.UserCreateDone.as_view(), name='signup_done'),
     path('signup/complete/<token>/', views.UserCreateComplete.as_view(), name='signup_complete'),
+    path('reset_password/', views.ResetPassword.as_view(), name='reset_password'),
+    path('reset_password/done/', views.ResetPasswordDone.as_view(), name='reset_password_done'),
+    path('reset_password/<uidb64>/<token>/', views.ResetPasswordConfirm.as_view(), name='reset_password_confirm'),
+    path('reset_password/complete/', views.ResetPasswordComplete.as_view(), name='reset_password_complete'),
 ]
